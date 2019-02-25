1a313792213cb2283417debd22ae5be075d6eceb
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 20:54:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,12 @@ holder_array = []
 name_hash.each do |key, value|
 holder_array << value
 end 
-holder_array.each_with_index do |index, value|
-if holder_array[index-1] == nil || holder_array[index] == nil
+until holder_array.length = 1
+if holder_array[i] == nil || holder_array[i-1] == nil
   nil 
-elsif holder_array[index-1] < holder_array[index]
+elsif holder_array[i] < holder_array[i-1]
   holder_array.delete(holder_array[index]) 
-elsif holder_array[index-1] > holder_array[index]
+elsif holder_array[i] < holder_array[i-1]
   holder_array.delete(holder_array[index-1]) 
 end 
 end 
