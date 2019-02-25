87dd73c8011aefee29f3f224b2f76cfebe233e23
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 21:06:26 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  i = 0
+holder_array = []
+name_hash.each do |key, value|
+holder_array << value
+end 
 
-end
+while holder_array.length > 1 do
+if holder_array[i] < holder_array[i+1]
+  holder_array.delete(holder_array[i+1]) 
+elsif holder_array[i] > holder_array[i+1]
+  holder_array.delete(holder_array[i]) 
+end 
+end 
+name_hash.key(holder_array[0])
+end 
