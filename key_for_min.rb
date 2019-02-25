00409d82a434fdfae490792854cebd574fef3c28
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 19:22:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_key = ""
 name_hash.each do |key, value|
   
-  return key
+lowest_key = key
 end
+lowest_key
 end
