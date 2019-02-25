fda230da99996b5fd3a66a3d119c022bf5a3d01c
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 19:42:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,11 @@
 require "pry"
 
 def key_for_min_value(name_hash)
+holder_array = []
 name_hash.each do |key, value|
- if value 
+holder_array << value
+end 
+holder_array.each do |value|
+  value <=> 
 end 
 end
