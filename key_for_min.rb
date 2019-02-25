b1fbc3bc42c29c73a2bd34ac60fbaac4e948c14a
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 19:40:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,15 +1,7 @@
 require "pry"
 
 def key_for_min_value(name_hash)
-  lowest_key = ""
-  array_sorted = []
 name_hash.each do |key, value|
-  array_sorted << value
-  if array_sorted[i] < array_sorted[i-1]
-    lower_value = array_sorted[i] 
+ if value 
 end 
-end 
-lowest_key = name_hash[array_sort[0]]
-binding.pry
-lowest_key
 end
