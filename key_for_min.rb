3060fe53758f1e21a3090b0bfc7c181a85a45f66
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 19:37:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,6 @@ def key_for_min_value(name_hash)
 name_hash.each do |key, value|
   array_sort << value
 end 
-array_sort.collect do |a, b|
-if a == b
-0
-elsif a < b
--1
-elsif a > b
-1
-end
 end 
 lowest_key = name_hash[array_sort[0]]
 binding.pry
