44e02f23d9b7b3ebae983e41ae891c6928e09f7e
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-25 19:51:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ name_hash.each do |key, value|
 holder_array << value
 end 
 holder_array.each do |a, b|
-if a.includes?(nil) || b.includes?(nil)
+if a.include?(nil) || b.include?(nil)
   nil 
 a < b 
   holder_array.delete(b) 
